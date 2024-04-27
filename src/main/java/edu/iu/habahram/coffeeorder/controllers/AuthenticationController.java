051b1374ce049574616eea467852fa8973f3345e
mcintyrb <bmac0607@gmail.com>
2024-04-26 21:50:24 -0400

    homework 10

---
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/AuthenticationController.java
@@ -0,0 +1,52 @@
+package edu.iu.habahram.coffeeorder.controllers;
+
+import edu.iu.habahram.coffeeorder.model.Customer;
+import edu.iu.habahram.coffeeorder.repository.CustomerFileRepository;
+import edu.iu.habahram.coffeeorder.security.TokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    CustomerFileRepository customerRepository;
+    public AuthenticationController(AuthenticationManager
+                                            authenticationManager,
+                                    TokenService tokenService,
+                                    CustomerFileRepository
+                                            customerFileRepository) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+        this.customerRepository = customerFileRepository;
+    }
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+            String passwordEncoded = bc.encode(customer.getPassword());
+            customer.setPassword(passwordEncoded);
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/signin")
+    public String login(@RequestBody Customer customer) {
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                customer.getUsername()
+                                , customer.getPassword()));
+
+        return tokenService.generateToken(authentication);
+    }
+}
